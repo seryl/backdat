4b875fee1166bbfdd03ce51399096f087c47f03a
Josh Toft <joshtoft@gmail.com>
2012-08-20 16:57:39 -0700

    Add link_spec

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -8,4 +8,32 @@ describe "Backdat::Link" do
   after(:each) do
     @link = nil
   end
+
+  it "should have a default name of `Link`" do
+    @link.name.should eql("Link")
+  end
+
+  it "should support fall-thru parameter checking at initialization" do
+    @link = Backdat::Link.new(:bleh => "awesome")
+    @link.link_config(:bleh).should eql("awesome")
+  end
+
+  it "should support fall-thru parameter checking" do
+    @link.link_config(:blehk).should eql(nil)
+    Backdat::Config[:link_blehk] = "cool"
+    @link.link_config(:blehk).should eql("cool")
+    Backdat::Config.configuration.delete(:link_blehk)
+  end
+
+  it "should default to a `:file` format" do
+    @link.format.should eql(:file)
+  end
+
+  it "should have nil as a default next" do
+    @link.next.should eql(nil)
+  end
+
+  it "should have nil as a default before" do
+    @link.before.should eql(nil)
+  end
 end
