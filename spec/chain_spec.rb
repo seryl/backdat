2325d63de8da13c7e04d7dbf6a44bada33e4fbe1
Josh Toft <joshtoft@gmail.com>
2012-08-20 16:51:09 -0700

    Update chain spec

--- spec/chain_spec.rb
+++ spec/chain_spec.rb
@@ -8,4 +8,43 @@ describe "Backdat::Chain" do
   after(:each) do
     @chain = nil
   end
+
+  it "should be able to add a single link" do
+    link = Backdat::Link.new
+    @chain.add(link)
+    @chain.root.should eql(link)
+    @chain.tail.should eql(link)
+  end
+
+  it "should be able to add two links" do
+    link1 = Backdat::Link.new
+    link2 = Backdat::Link.new
+    @chain.add(link1)
+    @chain.add(link2)
+    @chain.root.should eql(link1)
+    @chain.tail.should eql(link2)
+  end
+
+  it "should be able to add an arbitrary number of links" do
+    link1 = Backdat::Link.new
+    link2 = Backdat::Link.new
+    link3 = Backdat::Link.new
+    @chain.add(link1)
+    @chain.add(link2)
+    @chain.add(link3)
+    @chain.root.should eql(link1)
+    @chain.tail.should eql(link3)
+  end
+
+  it "should be able to remove a link" do
+    link1 = Backdat::Link.new
+    link2 = Backdat::Link.new
+    link3 = Backdat::Link.new
+    @chain.add(link1)
+    @chain.add(link2)
+    @chain.add(link3)
+    @chain.remove(link3)
+    @chain.root.should eql(link1)
+    @chain.tail.should eql(link2)
+  end
 end
