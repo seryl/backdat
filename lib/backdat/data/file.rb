9ec2cae3f66781978f067e9e3fb28a1f1e127807
Josh Toft <joshtoft@gmail.com>
2012-08-22 18:22:24 -0700

    Add :path as an alias for @items

--- lib/backdat/data/file.rb
+++ lib/backdat/data/file.rb
@@ -1,3 +1,5 @@
 # The file data enumerator.
 class Backdat::Data::File < Backdat::Data::Base
+  # Path alias for the list of items.
+  def path; @items end
 end
--- lib/backdat/data/stream.rb
+++ lib/backdat/data/stream.rb
@@ -1,3 +1,5 @@
 # The stream data enumerator.
 class Backdat::Data::Stream < Backdat::Data::Base
+  # Path alias for the list of items.
+  def path; @items end
 end
